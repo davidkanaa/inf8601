e144c65f2679d8bcfa088ea1f3c29e607cfaf18c
davidkanaa <david.kanaa@gmail.com>
2017-09-21 13:35:18 -0400

    Trying to make it work again with TidMap.

--- Lab1/src/dragon_tbb.cpp
+++ Lab1/src/dragon_tbb.cpp
@@ -65,7 +65,7 @@ public:
 		// int id = 0;
 		// while (data.tid[id] != 0) id++;
 		// data.tid[id] = 1;	// set this thread to BUSY.
-		int id = tidMap.getIdFromTid(gettid());
+		int id = tidMap->getIdFromTid(gettid());
 		dragon_draw_raw(r.begin(), r.end(), data.dragon, data.dragon_width, data.dragon_height, data.limits, static_cast<uint64_t>(id));
 		// data.tid[id] = 0;	// set this thread to FREED.
 	}
